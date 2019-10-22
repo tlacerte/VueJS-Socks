2d648701183b1a13fe7b5466087ce764345f6442
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-22 17:26:29 -0500

    add to cart, hover over images

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        image: './vmSocks-green-onWhite.jpg'
+      product: 'Socks',
+      image: './vmSocks-green-onWhite.jpg',
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: './vmSocks-green-onWhite.jpg'
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: './vmSocks-blue-onWhite.jpg'
+        }
+      ],
+      cart: 0
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      },
+      removeFromCart() {
+        this.cart -= 1
+      }
     }
-})
+  })
+  
+  
