42508343f8f5b3cb9abf63e7874483b8b9cb32bf
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-22 17:53:51 -0500

    hover in stock out of stock

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
+      brand: 'Vue Mastery',
       product: 'Socks',
-      image: './vmSocks-green-onWhite.jpg',
-      inStock: true,
+      selectedVariant: 0,
       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
       variants: [
         {
           variantId: 2234,
           variantColor: 'green',
-          variantImage: './vmSocks-green-onWhite.jpg'
+          variantImage: './vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
         },
         {
           variantId: 2235,
           variantColor: 'blue',
-          variantImage: './vmSocks-blue-onWhite.jpg'
+          variantImage: './vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
         }
       ],
       cart: 0
@@ -23,12 +25,23 @@ var app = new Vue({
       addToCart() {
         this.cart += 1
       },
-      updateProduct(variantImage) {
-        this.image = variantImage
+      updateProduct(index) {
+        this.selectedVariant = index
       },
       removeFromCart() {
         this.cart -= 1
       }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        }
     }
   })
   
